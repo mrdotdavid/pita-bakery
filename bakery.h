44287364950b5b25f4ad5ce5b8d2292bc378a3f1
David Barrios <mrdotdavid@gmail.com>
2021-02-27 00:45:01 -0800

    Contains Bakery class with function prototypes

---
+++ bakery.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Bakery {
+    float calcSmallPitasTime(float qty);
+    float calcMediumPitasTime(float qty);
+    float calcLargePitasTime(float qty);
+    void calcTotalTime();
+    void start();
+};
