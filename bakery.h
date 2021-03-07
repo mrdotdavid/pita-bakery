2ca13fbdc95d83dc52d51993d0ed05808e799092
David Barrios <mrdotdavid@gmail.com>
2021-03-07 11:45:22 -0800

    Added variables inside struct instead of inside of functions

--- bakery.h
+++ bakery.h
@@ -1,6 +1,17 @@
 #pragma once
 
 struct Bakery {
-    float calcPitaTime(float, float, float, float&, float&, float&);
-    void readOrder();
+    float   smallTrays, mediumTrays, largeTrays;
+    float   smallQty, mediumQty, largeQty;
+    float   smallHours, smallMinutes, smallTime;
+    float   mediumHours, mediumMinutes, mediumTime;
+    float   largeHours, largeMinutes, largeTime;
+    float   totalHours, totalMinutes, totalTime; 
+    enum    pitaSize { small, medium, large, total };
+
+    void    menu();
+    void    readOrder();
+    float   calcPitaTime(float, float, float, float&, float&, float&, float&);
+    bool    isOverHour(float, float, float, pitaSize = total);
+
 };
